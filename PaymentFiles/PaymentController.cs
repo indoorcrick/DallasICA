8e2e328d5600366c07a723c3bceccb35280bac83
indoorcrick <kithir@gmail.com>
2014-12-15 09:40:13 +0530

    Payment Related Files

---
+++ PaymentFiles/PaymentController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using System.IO;
+using System.Net;
+namespace DallasICA.Controllers
+{
+    public class PaymentController : Controller
+    {
+        #region Payment Misc
+        [Authorize]
+        public ActionResult PaymentMisc(string packageName, double price, string packageDesc)
+        {
+            Session["PackageName"] = packageName;
+            Session["Price"] = price.ToString("N2");
+            Session["PackageDesc"] = packageDesc;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult PaymentMisc()
+        {
+            Guid regID = Guid.NewGuid();
+            string orderID = "0001";//PayPalHelper.GetOrderID();
+
+            string packageName = Session["PackageName"].ToString();
+            string price = Session["Price"].ToString();
+
+            string message = string.Format("{0} | {1}", packageName, Session["PackageDesc"].ToString());
+
+            Session["RegistrationID"] = regID;
+            Session["Paypal_PackageName"] = message;
+            Session["Paypal_Price"] = price;
+            Session["Paypal_OrderId"] = orderID;
+            return View("Payment");
+        }
+        #endregion
+
+        public ActionResult Payment()
+        {
+            return View();
+        }
+
+        #region Payment Success
+
+        public string Status, TransID, OrderID, Email, CurrencyType, Amount, ProfileId, TransDate, plannumber, planName;
+
+        public ActionResult PaymentSuccess()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (Session["RegistrationID"] != null)
+                sb.Append(" [RegistrationID: " + Session["RegistrationID"].ToString() + "]");
+            if (Session["Paypal_OrderId"] != null)
+                sb.Append(" [Paypal_OrderId: " + Session["Paypal_OrderId"].ToString() + "]");
+            if (Session["Paypal_PackageName"] != null)
+                sb.Append(" [Paypal_PackageName: " + Session["Paypal_PackageName"].ToString() + "]");
+
+
+            string Payment_Status = string.Empty;
+            if (Request.QueryString["payment_status"] != null)
+                Payment_Status = Request.QueryString["payment_status"].ToString();
+
+            if (Payment_Status != string.Empty)
+            {
+                sb.Append(" [payment_status: " + Payment_Status + "]");
+
+                StringBuilder strReturnQstring = new StringBuilder();
+                strReturnQstring.Append(Request.QueryString.ToString());
+                strReturnQstring.Append("&cmd=_notify-validate");
+
+
+                string PostMode = "2";
+                string WebURL = string.Empty;
+                string SdHost = string.Empty;
+                
+                if (PostMode == "1")
+                {
+                    WebURL = "http://www.paypal.com/cgi-bin/webscr";
+                    SdHost = "www.paypal.com";
+                }
+                else if (PostMode == "2")
+                {
+                    WebURL = "https://www.paypal.com/cgi-bin/webscr";
+                    SdHost = "www.paypal.com";
+                }
+                else
+                {
+                    Response.Write("PostMode: " + (PostMode) + "is invalid!");
+                }
+
+                
+                HttpWebRequest myRequest = (HttpWebRequest)HttpWebRequest.Create(WebURL);
+                myRequest.AllowAutoRedirect = false;
+                myRequest.Method = "POST";
+                myRequest.ContentType = "application/x-www-form-urlencoded";
+
+                //Create post stream 
+                Stream RequestStream = myRequest.GetRequestStream();
+                byte[] SomeBytes = Encoding.UTF8.GetBytes(strReturnQstring.ToString());
+                RequestStream.Write(SomeBytes, 0, SomeBytes.Length);
+                RequestStream.Close();
+
+                sb.Append(" [request sent]");
+
+                //Send request and get response 
+                HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+                if (myResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    sb.Append(" [response: OK]");
+
+                    //Get the stream. 
+                    Stream ReceiveStream = myResponse.GetResponseStream();
+                    Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                    //send the stream to a reader. 
+                    StreamReader readStream = new StreamReader(ReceiveStream, encode);
+                    //Read the result 
+                    string Result = readStream.ReadLine();
+                    if (Result == "INVALID")
+                    {
+                        sb.Append(" [result: INVALID]");
+
+                        return View("PaymentCancel");
+                    }
+                    // The result was invalid so send a failure notice or some other handling. 
+                    else if (Result == "VERIFIED")
+                    {
+                        sb.Append(" [result: VERIFIED]");
+
+                        switch ((Payment_Status))
+                        {
+                            case "Completed":
+
+                                if (Session["RegistrationID"] != null)
+                                {
+                                    //Guid regID = new Guid(Session["RegistrationID"].ToString());
+                                   // m_bl.AddPaymentDetails(regID, Session["Paypal_OrderId"].ToString(), Session["Paypal_TeamName"].ToString(), Payment_Status, DateTime.Now);
+                                }
+                                else
+                                {
+                                    //Guid regID = Guid.Empty;
+                                   // m_bl.AddPaymentDetails(regID, Session["Paypal_OrderId"].ToString(), Session["Paypal_TeamName"].ToString(), Payment_Status, DateTime.Now);
+                                }
+
+                                break;
+                        }
+                    }
+                }
+            }
+
+           // m_bl.LogInfo("PayPalSuccess", sb.ToString());
+
+            return View();
+
+        }
+
+        #endregion
+
+        public ActionResult PaymentCancel()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (Session["RegistrationID"] != null)
+                sb.Append(" [RegistrationID: " + Session["RegistrationID"].ToString() + "]");
+            if (Session["Paypal_OrderId"] != null)
+                sb.Append(" [Paypal_OrderId: " + Session["Paypal_OrderId"].ToString() + "]");
+            if (Session["Paypal_PackageName"] != null)
+                sb.Append(" [Paypal_PackageName: " + Session["Paypal_PackageName"].ToString() + "]");
+            if (Session["Paypal_Price"] != null)
+                sb.Append(" [Paypal_Price: " + Session["Paypal_Price"].ToString() + "]");
+
+            if (sb.Length == 0)
+                sb.Append("No Session data found!");
+
+           // m_bl.LogInfo("PayPalCancel", sb.ToString());
+            return View();
+        }
+    }
+}
---
+++ PaymentFiles/PlayerPackageController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DallasICA;
+using DallasICA.Context;
+using Microsoft.AspNet.Identity;
+
+namespace DallasICA.Controllers
+{
+    public class PlayerPackageController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /PlayerPackage/
+        public async Task<ActionResult> Index()
+        {
+
+            //Get specific player package based on userid
+            var userId = User.Identity.GetUserId();
+            var playerList = db.PLAYERs.Where(x => x.UserId == userId);
+
+            IQueryable<PLAYER_PACKAGE> player_package = null;
+            if(playerList.Any())
+            {
+                 player_package =  db.PLAYER_PACKAGE.Where(p => p.PLAYER.UserId == userId);
+            }
+             else
+            {
+                player_package =  (db.PLAYER_PACKAGE);
+            }
+          //return View(await  ((playerList.Any()) ? (db.PLAYER_PACKAGE.Include(p => p.PLAYER.UserId == userId)).ToListAsync()
+          //      : (db.PLAYER_PACKAGE).ToListAsync()));
+         
+            return View(await player_package.ToListAsync());
+        }
+
+        // GET: /PlayerPackage/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PLAYER_PACKAGE player_package = await db.PLAYER_PACKAGE.FindAsync(id);
+            if (player_package == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player_package);
+        }
+
+        // GET: /PlayerPackage/Create
+        public ActionResult Create()
+        {
+            ViewBag.PLAYER_ID = new SelectList(db.PLAYERs, "PLAYER_ID", "FIRST_NAME");
+            return View();
+        }
+
+        // POST: /PlayerPackage/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="ID,PLAYER_ID,PACKAGE_ID,START_DATE,END_DATE,COMMENTS,ACTIVE,SSMA_TimeStamp")] PLAYER_PACKAGE player_package)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PLAYER_PACKAGE.Add(player_package);
+                await db.SaveChangesAsync();
+               // return RedirectToAction("Index");
+               
+                return RedirectToAction("Index");
+            }
+
+           // ViewBag.PLAYER_ID = new SelectList(db.PLAYERs, "PLAYER_ID", "FIRST_NAME", player_package.PLAYER_ID);
+           
+            return View(player_package);
+        }
+
+        // GET: /PlayerPackage/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PLAYER_PACKAGE player_package = await db.PLAYER_PACKAGE.FindAsync(id);
+            if (player_package == null)
+            {
+                return HttpNotFound();
+            }
+           // ViewBag.PLAYER_ID = new SelectList(db.PLAYERs, "PLAYER_ID", "FIRST_NAME", player_package.PLAYER_ID);
+            //ViewBag.PACKAGE_ID = new SelectList(db.PACKAGEs, "PACKAGE_ID", "PACKAGE_NAME", player_package.PACKAGE_ID);
+            ViewBag.PACKAGE_ID = db.PACKAGEs.Select(p => new SelectListItem()
+            {
+                Text = p.PACKAGE_NAME,
+                Value = p.PACKAGE_ID.ToString()
+            });
+           
+            return View(player_package);
+        }
+
+        // POST: /PlayerPackage/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="ID,PLAYER_ID,PACKAGE_ID,START_DATE,END_DATE,COMMENTS,ACTIVE,SSMA_TimeStamp")] PLAYER_PACKAGE player_package)
+        {
+            if (ModelState.IsValid)
+            {
+                PLAYER_PACKAGE existing = await db.PLAYER_PACKAGE.FindAsync(player_package.ID);
+                existing.PACKAGE_ID = player_package.PACKAGE_ID;
+                existing.START_DATE = player_package.START_DATE;
+                existing.END_DATE = player_package.END_DATE;
+                existing.COMMENTS = player_package.COMMENTS;
+                
+                await db.SaveChangesAsync();
+               // return RedirectToAction("Index");
+                //-- Getting Package Information
+                var pack = db.PACKAGEs.Where(p => p.PACKAGE_ID == player_package.PACKAGE_ID).First();
+                //-- Redirect to Payment Page
+                return RedirectToAction("PaymentMisc", "Payment", new { packageName = pack.PACKAGE_NAME, price = pack.PACKAGE_PRICE, packageDesc = pack.PACKAGE_DESC });
+            }
+           // ViewBag.PLAYER_ID = new SelectList(db.PLAYERs, "PLAYER_ID", "FIRST_NAME", player_package.PLAYER_ID);
+            ViewBag.PACKAGE_ID = new SelectList(db.PACKAGEs, "PACKAGE_ID", "PACKAGE_NAME", player_package.PACKAGE_ID);
+           return View(player_package);
+           
+        }
+
+        // GET: /PlayerPackage/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PLAYER_PACKAGE player_package = await db.PLAYER_PACKAGE.FindAsync(id);
+            if (player_package == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player_package);
+        }
+
+        // POST: /PlayerPackage/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            PLAYER_PACKAGE player_package = await db.PLAYER_PACKAGE.FindAsync(id);
+            db.PLAYER_PACKAGE.Remove(player_package);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
